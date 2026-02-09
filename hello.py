ec46ed8a2820234e94d1724b7fdffa8bc66418f8
Ayaindeed <aya.mechouahiriffi@usmba.ac.ma>
2026-02-09 21:01:07 +0100

    1st comm/

---
+++ hello.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta 
+
+default_args = {
+    'owner': 'datamasterylab',
+    'start_date' : datetime(2026, 9, 2),
+    'catchup' : 'False'
+}
+
+dag = DAG (
+    'hello_world',
+    default_args = default_args,
+    schedule = timedelta(days = 1)
+)
+
+t1 = BashOperator(
+    task_id = 'print_hello',
+    bash_command = 'echo "Hello World"',
+    dag = dag
+)
+
+t1 = BashOperator(
+    task_id = 'print_dml',
+    bash_command = 'echo "Hello World"',
+    dag = dag
+)
